f2f121c0a8414834d9d789c631b1349eb7d11cb8
pulvariser <mayurburange72@gmail.com>
2024-10-04 01:06:54 +0530

    dot change

--- client/src/components/about-us/index.jsx
+++ client/src/components/about-us/index.jsx
@@ -24,7 +24,7 @@ const  About  = () => {
       },
       {  
         info:'study :',
-        value:'R.T.M Nagpur University'
+        value:'R.T.M. Nagpur University'
       },
       {  
        info:'work status :',
